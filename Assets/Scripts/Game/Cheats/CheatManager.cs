83b9005cb8adcc4557abda1b10e070e1f88a6629
Grant Shotwell <gcshotwell@gmail.com>
2025-08-11 22:42:04 -0700

    Remove redundant lists and use ICheat instead

--- Assets/Scripts/Game/Cheats/CheatManager.cs
+++ Assets/Scripts/Game/Cheats/CheatManager.cs
@@ -1,32 +1,27 @@
 ﻿using Assets.Scripts.Game.Cheats;
-using System.Collections.Generic;
 using Zenject;
 
 namespace Game.Cheats {
 	public class CheatManager {
 
-		private ICollection<IPlayerHealthCheatable> PlayerHealthCheatables { get; set; } = new List<IPlayerHealthCheatable>();
-
 		[field: Inject]
 		public EnemyHealthCheat EnemyHealthCheat { get; private set; }
 
-		private ICollection<IEnemyHealthCheatable> EnemyHealthCheatables { get; set; } = new List<IEnemyHealthCheatable>();
-
 		public void Register(object cheatable) {
 			if (cheatable is IPlayerHealthCheatable playerHealthCheatable) {
-				PlayerHealthCheatables.Add(playerHealthCheatable);
+				// TODO
 			}
 			if (cheatable is IEnemyHealthCheatable enemyHealthCheatable) {
-				EnemyHealthCheatables.Add(enemyHealthCheatable);
+				EnemyHealthCheat.OnRegistered(enemyHealthCheatable);
 			}
 		}
 
 		public void Unregister(object cheatable) {
 			if (cheatable is IPlayerHealthCheatable playerHealthCheatable) {
-				PlayerHealthCheatables.Remove(playerHealthCheatable);
+				// TODO
 			}
 			if (cheatable is IEnemyHealthCheatable enemyHealthCheatable) {
-				EnemyHealthCheatables.Remove(enemyHealthCheatable);
+				EnemyHealthCheat.OnUnregistered(enemyHealthCheatable);
 			}
 		}
 
