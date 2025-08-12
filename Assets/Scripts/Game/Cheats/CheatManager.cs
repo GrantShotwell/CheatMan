6f98df27d3dd14fa1bb76630d06a8d7a58eb03b6
Grant Shotwell <gcshotwell@gmail.com>
2025-08-11 20:22:19 -0700

    Bind EnemyHealthCheats

--- Assets/Scripts/Game/Cheats/CheatManager.cs
+++ Assets/Scripts/Game/Cheats/CheatManager.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using Assets.Scripts.Game.Cheats;
+using System.Collections.Generic;
+using Zenject;
 
 namespace Game.Cheats {
 	public class CheatManager {
 
 		private ICollection<IPlayerHealthCheatable> PlayerHealthCheatables { get; set; } = new List<IPlayerHealthCheatable>();
+
+		[field: Inject]
+		public EnemyHealthCheat EnemyHealthCheat { get; private set; }
 
 		private ICollection<IEnemyHealthCheatable> EnemyHealthCheatables { get; set; } = new List<IEnemyHealthCheatable>();
 
--- Assets/Scripts/RootInstaller.cs
+++ Assets/Scripts/RootInstaller.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Game.Cheats;
 using Game;
 using Game.Cheats;
 using UnityEngine;
@@ -8,10 +9,16 @@ public class RootInstaller : MonoInstaller {
 	[SerializeField] private GameObject _exampleEnemyPrefab;
 
 	public override void InstallBindings() {
-		Container.BindInterfacesAndSelfTo<CheatManager>()
-			.AsSingle();
+		InstallCheats();
 		Container.BindFactory<ExampleEnemy, ExampleEnemy.Factory>()
 			.FromComponentInNewPrefab(_exampleEnemyPrefab)
+			.AsSingle();
+	}
+
+	private void InstallCheats() {
+		Container.BindInterfacesAndSelfTo<CheatManager>()
+			.AsSingle();
+		Container.BindInterfacesAndSelfTo<EnemyHealthCheat>()
 			.AsSingle();
 	}
 
