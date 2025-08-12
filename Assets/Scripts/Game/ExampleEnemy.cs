8c27856b7b907f10caf10efffb319a56f0026c76
Grant Shotwell <gcshotwell@gmail.com>
2025-08-11 20:06:04 -0700

    Add more to example enemy

--- Assets/Scripts/Game/ExampleEnemy.cs
+++ Assets/Scripts/Game/ExampleEnemy.cs
@@ -1,10 +1,26 @@
-﻿using UnityEngine;
+﻿using Game.Cheats;
+using UnityEngine;
+using Zenject;
 
 namespace Game {
-	public class ExampleEnemy : IEnemyHealthCheatable {
+	public class ExampleEnemy : MonoBehaviour, IEnemyHealthCheatable {
+		[SerializeField] private readonly CheatManager _cheatManager;
 
 		[field: SerializeField]
 		public AdjustableNumber Health { get; private set; } = new(10f);
 
+		private void Update() {
+			Debug.Log($"My health is {Health}!");
+		}
+
+		private void OnEnable() {
+			_cheatManager.Register(this);
+		}
+
+		private void OnDisable() {
+			_cheatManager.Unregister(this);
+		}
+
+		public class Factory : PlaceholderFactory<ExampleEnemy> { }
 	}
 }
--- Assets/Scripts/RootInstaller.cs
+++ Assets/Scripts/RootInstaller.cs
@@ -1,4 +1,18 @@
+using Game;
+using Game.Cheats;
 using UnityEngine;
+using Zenject;
 
-public class RootInstaller : MonoBehaviour {
+public class RootInstaller : MonoInstaller {
+
+	[SerializeField] private GameObject _exampleEnemyPrefab;
+
+	public override void InstallBindings() {
+		Container.BindInterfacesAndSelfTo<CheatManager>()
+			.AsSingle();
+		Container.BindFactory<ExampleEnemy, ExampleEnemy.Factory>()
+			.FromComponentInNewPrefab(_exampleEnemyPrefab)
+			.AsSingle();
+	}
+
 }
