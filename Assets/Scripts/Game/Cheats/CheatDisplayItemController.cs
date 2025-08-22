e948809c4debe04bf407a94d11b75d75e500f6b3
Grant Shotwell <gcshotwell@gmail.com>
2025-08-21 23:55:03 -0700

    Theoretical skeleton of cheat display

---
+++ Assets/Scripts/Game/Cheats/CheatDisplayItemController.cs
@@ -0,0 +1,22 @@
+﻿using Cysharp.Threading.Tasks;
+using Game.Cheats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Cheats {
+	public class CheatDisplayItemController : MonoBehaviour {
+
+		[SerializeField] private TextMeshProUGUI _text;
+
+		public async UniTask RunActivation(CheatActivation activation, CancellationToken cancellationToken = default) {
+			_text.text = activation.Cheat.ToString();
+		}
+
+	}
+}
