0a6365e27211c1810789f7c0e4b7b932e003df74
Grant Shotwell <gcshotwell@gmail.com>
2025-08-23 13:39:55 -0700

    Add art to prefabs

---
+++ Assets/Scripts/Game/Levels/Obstacles/MovingCarObstacle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Levels.Obstacles {
+	public class MovingCarObstacle : Obstacle {
+	}
+}
