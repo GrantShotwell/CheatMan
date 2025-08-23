49ea9a2deeb3a43fae9ac6b34d997d79aef24604
Grant Shotwell <gcshotwell@gmail.com>
2025-08-23 15:24:52 -0700

    Base class for Enemy and Obstacle

---
+++ Assets/Scripts/Game/Levels/LevelEntity.cs
@@ -0,0 +1,84 @@
+﻿using Assets.Scripts.Game.Levels.Enemies;
+using Cysharp.Threading.Tasks;
+using Game.Cheats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Zenject;
+
+namespace Game.Levels {
+	public class LevelEntity : MonoBehaviour, ICheatable {
+		[Inject] protected readonly DiContainer _container;
+		[Inject] protected readonly CheatManager _cheatManager;
+		[Inject] protected readonly PlayerController _player;
+		private CancellationTokenSource _cts;
+		private StateFunction _currentState;
+
+		private bool _seenByPlayerDirty = true;
+		private bool _seenByPlayer = false;
+
+		protected delegate UniTask StateFunction(CancellationToken cancellationToken);
+
+		public bool SeenByPlayer {
+			get {
+				if (!_seenByPlayerDirty) return _seenByPlayer;
+				_seenByPlayerDirty = false;
+				return _seenByPlayer = GetSeenByPlayer();
+			}
+		}
+
+		protected virtual void Awake() {
+		}
+
+		protected virtual void Start() {
+			_cheatManager.Register(this);
+		}
+
+		protected virtual void OnDestroy() {
+			_cheatManager.Unregister(this);
+		}
+
+		protected virtual void FixedUpdate() {
+			_seenByPlayerDirty = true;
+		}
+
+		protected void SetState(StateFunction fn, CancellationToken cancellationToken = default) {
+			if (fn == _currentState) return;
+			ResetState(fn, cancellationToken);
+		}
+
+		protected void ResetState(StateFunction fn, CancellationToken cancellationToken = default) {
+			RefreshCts(cancellationToken);
+			_currentState = fn;
+			RunState(fn, _cts.Token).Forget();
+		}
+
+		private void RefreshCts(CancellationToken cancellationToken = default) {
+			_cts?.Cancel();
+			_cts?.Dispose();
+			_cts = CancellationTokenSource.CreateLinkedTokenSource(destroyCancellationToken, cancellationToken);
+		}
+
+		private async UniTask RunState(StateFunction fn, CancellationToken cancellationToken = default) {
+			while (!cancellationToken.IsCancellationRequested) {
+				try {
+					await fn(cancellationToken);
+				} catch (Exception exception) {
+					if (exception is OperationCanceledException) continue;
+					Debug.LogException(exception);
+				}
+				await UniTask.NextFrame(PlayerLoopTiming.Update);
+			}
+		}
+
+		protected virtual bool GetSeenByPlayer() {
+			return Vector2.Distance(_player.transform.position, transform.position) < 100f;
+		}
+
+	}
+}
--- Assets/Scripts/Game/Levels/Obstacles/Obstacle.cs
+++ Assets/Scripts/Game/Levels/Obstacles/Obstacle.cs
@@ -1,11 +1,26 @@
-﻿using Game.Cheats;
+﻿using Cysharp.Threading.Tasks;
+using Game.Cheats;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace Game.Levels.Obstacles {
-	public abstract class Obstacle : MonoBehaviour, ICheatable {
+	public abstract class Obstacle : LevelEntity, ICheatable {
 
 		[field: SerializeField, FormerlySerializedAs("contactDamage")] public AdjustableNumber contactDamage { get; private set; } = new(1);
+		public float currentHealth = 1f;
+
+		protected override void FixedUpdate() {
+			base.FixedUpdate();
+			if (currentHealth <= 0) {
+				SetState(DeathState);
+			}
+		}
+
+		protected virtual UniTask DeathState(CancellationToken cancellationToken) {
+			Destroy(gameObject);
+			return UniTask.CompletedTask;
+		}
 
 		public virtual float GetContactDamage(PlayerController player, float health, out bool hit) {
 			Debug.Log(health);
@@ -13,6 +28,16 @@ namespace Game.Levels.Obstacles {
 			hit = damage > 0f;
 			return Mathf.CeilToInt(contactDamage);
 		}
+
+		public void DealDamage(float damage) {
+			currentHealth -= damage;
+			OnDamaged();
+			if (currentHealth <= 0f) {
+				SetState(DeathState);
+			}
+		}
+
+		protected virtual void OnDamaged() { }
 
 	}
 }
