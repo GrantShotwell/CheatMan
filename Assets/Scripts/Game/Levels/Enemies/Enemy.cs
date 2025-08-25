cd0c4e7f9968cef67a68980a1eed5824aaed3080
Grant Shotwell <gcshotwell@gmail.com>
2025-08-24 23:10:45 -0700

    Death particles

--- Assets/Scripts/Game/Levels/Enemies/Enemy.cs
+++ Assets/Scripts/Game/Levels/Enemies/Enemy.cs
@@ -1,27 +1,38 @@
-﻿using Assets.Scripts.Game.Levels.Enemies;
+﻿using Assets.Scripts.Game.Levels;
 using Cysharp.Threading.Tasks;
 using Game.Cheats;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Zenject;
 
 namespace Game.Levels.Enemies {
 	public abstract class Enemy : LevelEntity, ICheatable {
+		[Inject] private readonly ParticleSystemManager _particleSystemManager;
+		[SerializeField] private GameObject _particlesParent;
+		private DeathParticlesController _deathParticles;
+
+		private Vector2 _lastDamageDirection = Vector2.zero;
 
 		[field: SerializeField, FormerlySerializedAs("contactDamage")] public AdjustableNumber contactDamage { get; private set; } = new(1);
 		[field: SerializeField, FormerlySerializedAs("weaponDamage")] public AdjustableNumber weaponDamage { get; private set; } = new(1);
 		[field: SerializeField, FormerlySerializedAs("weaponSpeed")] public AdjustableNumber weaponSpeed { get; private set; } = new(5);
 		[field: SerializeField, FormerlySerializedAs("movementSpeed")] public AdjustableNumber movementSpeed { get; private set; } = new(1);
 		[field: SerializeField, FormerlySerializedAs("currentHealth")] public float currentHealth { get; set; } = 10;
+		[field: SerializeField] public float particlesAfterlife { get; private set; } = 10f;
+
+		protected override void Awake() {
+			_deathParticles = GetComponentInChildren<DeathParticlesController>();
+			base.Awake();
+		}
 
 		protected override void FixedUpdate() {
 			base.FixedUpdate();
-			if (currentHealth <= 0) {
-				SetState(DeathState);
-			}
 		}
 
 		protected virtual UniTask DeathState(CancellationToken cancellationToken) {
+			if (_deathParticles) _deathParticles.StartParticles(_lastDamageDirection);
+			if (_particlesParent) _particleSystemManager.KeepAlive(_particlesParent, particlesAfterlife);
 			Destroy(gameObject);
 			return UniTask.CompletedTask;
 		}
@@ -40,9 +51,13 @@ namespace Game.Levels.Enemies {
 			return Mathf.CeilToInt(contactDamage);
 		}
 
-		public void DealDamage(float damage) {
+		public void DealDamage(float damage, Vector2 direction) {
 			currentHealth -= damage;
+			_lastDamageDirection = direction;
 			OnDamaged();
+			if (currentHealth <= 0) {
+				SetState(DeathState);
+			}
 		}
 
 		protected virtual void OnDamaged() { }
