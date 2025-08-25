e61ecb2f98614e369db243d8fb686ccf12877e8d
Grant Shotwell <gcshotwell@gmail.com>
2025-08-24 17:46:44 -0700

    Car spawner

--- Assets/Scripts/Game/Levels/Obstacles/MovingCarObstacle.cs
+++ Assets/Scripts/Game/Levels/Obstacles/MovingCarObstacle.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
+using Game.Cheats;
+using System.Threading;
 using UnityEngine;
 
 namespace Game.Levels.Obstacles {
-	public class MovingCarObstacle : Obstacle {
+	public sealed class MovingCarObstacle : Obstacle {
+
+		[field: SerializeField] public AdjustableNumber drivingSpeed { get; private set; } = new(10f);
+
+		protected override void Start() {
+			base.Start();
+			SetState(DrivingState);
+		}
+
+		private async UniTask DrivingState(CancellationToken cancellationToken) {
+			while (!cancellationToken.IsCancellationRequested) {
+				await UniTask.NextFrame(PlayerLoopTiming.FixedUpdate, cancellationToken: cancellationToken);
+				transform.position += drivingSpeed * Time.fixedDeltaTime * Vector3.left;
+			}
+		}
+
 	}
 }
