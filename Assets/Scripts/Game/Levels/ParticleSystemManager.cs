3ed3eb404b4e66b61ac6a59d6455f3b59859f7af
Grant Shotwell <gcshotwell@gmail.com>
2025-08-24 19:03:28 -0700

    flask particles

---
+++ Assets/Scripts/Game/Levels/ParticleSystemManager.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Levels {
+	public sealed class ParticleSystemManager : MonoBehaviour {
+
+		public void KeepAlive(GameObject instance, float duration) {
+			instance.transform.parent = transform;
+			KeepAliveAsync(instance, duration, destroyCancellationToken).Forget();
+		}
+
+		private async UniTask KeepAliveAsync(GameObject instance, float duration, CancellationToken cancellationToken = default) {
+			await UniTask.WaitForSeconds(duration, cancellationToken: cancellationToken);
+			Destroy(instance);
+		}
+
+	}
+}
